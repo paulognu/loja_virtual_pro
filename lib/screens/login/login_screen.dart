2b767121a7b15fa667c14980028857cf62d47e48
João Paulo dos Santos Santos <paulo.gnu@gmail.com>
2024-12-15 00:08:44 -0300

    Implementação da validação do campo de email.

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual_pro/helpers/validators.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  LoginScreen({super.key});
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -15,60 +21,74 @@ class LoginScreen extends StatelessWidget {
       body: Center(
         child: Card(
           margin: EdgeInsets.all(16),
-          child: ListView(
-            padding: EdgeInsets.all(16),
-            shrinkWrap: true,
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "E-mail",
-                ),
-                keyboardType: TextInputType.emailAddress,
-                autocorrect: false,
-                validator: (email) {
-                  return null;
-                },
-              ),
-              SizedBox(height: 8),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Senha",
-                ),
-                obscureText: true,
-                validator: (pass) {
-                  if (pass == null || pass.isEmpty || pass.length < 6) {
-                    return "Senha inválida!";
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 16),
-              Align(
-                alignment: Alignment.centerRight,
-                child: InkWell(
-                  onTap: () {},
-                  child: Text(
-                    "Esqueci minha senha",
+          child: Form(
+            key: formKey,
+            child: ListView(
+              padding: EdgeInsets.all(16),
+              shrinkWrap: true,
+              children: [
+                TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    hintText: "E-mail",
                   ),
+                  keyboardType: TextInputType.emailAddress,
+                  autocorrect: false,
+                  validator: (email) {
+                    if (email == null || isValidEmail(email) == false) {
+                      return "E-mail inválido!";
+                    }
+
+                    return null;
+                  },
                 ),
-              ),
-              SizedBox(height: 16),
-              SizedBox(
-                height: 44,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor: WidgetStateProperty.all(primaryColor)),
-                  onPressed: () {},
-                  child: Text(
-                    "Entrar",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
+                SizedBox(height: 8),
+                TextFormField(
+                  controller: passController,
+                  decoration: InputDecoration(
+                    hintText: "Senha",
+                  ),
+                  obscureText: true,
+                  validator: (pass) {
+                    if (pass == null || pass.isEmpty || pass.length < 6) {
+                      return "Senha inválida!";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: InkWell(
+                    onTap: () {},
+                    child: Text(
+                      "Esqueci minha senha",
                     ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 16),
+                SizedBox(
+                  height: 44,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all(primaryColor)),
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        debugPrint(emailController.text);
+                        debugPrint(passController.text);
+                      }
+                    },
+                    child: Text(
+                      "Entrar",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
